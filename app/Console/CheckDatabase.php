9b5c8307c9854a1e016d609c171569677855cb7b
Lionel <liodu13170@gmail.com>
2020-04-21 20:58:48 +0200

    correction affichage

--- app/Console/CheckDatabase.php
+++ app/Console/CheckDatabase.php
@@ -8,11 +8,14 @@ class CheckDatabase extends Command {
 
 	public function exec() {
 
-		$tables = ['user_type', 'user, picture', 'charity_association', 'brand', 'celebrity_category', 'product_category'];
+		$tables = ['user_type', 'user', 'picture', 'charity_association', 'brand', 'celebrity_category', 'product_category'];
 
+		echo '<table border="2" cellpadding="2" cellspacing="2">';
+		echo '<tr><th>Table</th><th>Lignes</th></tr>';
 		foreach ($tables as $table) {
-			var_dump($table, $this->pdo->query('SELECT COUNT(*) FROM ' . $table)->fetchColumn());
+			echo '<tr><td>' . $table . '</td><td>' . $this->pdo->query('SELECT COUNT(*) FROM ' . $table)->fetchColumn() . '</td></tr>';
 		}
+		echo '</table>';
 	}
 
 }
